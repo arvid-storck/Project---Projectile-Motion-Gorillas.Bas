17bb2e4e0ac1923343306d6ae7959c36938a66f1
arvid-storck <37932528+arvid-storck@users.noreply.github.com>
2024-05-14 19:40:12 +0200

    Småfix

--- Projekt.py
+++ Projekt.py
@@ -8,22 +8,21 @@ class plot:
         plt.rcParams['toolbar'] = 'None'
         self.fig, self.ax = plt.subplots()
         self.line=[]
-        
         self.ax.set(xlim=(xmin, xmax), ylim=(ymin, ymax))
-        
+        self.show()
         
         
 
-    def addLine(self,x,y):
+    def addLine(self,x,y,color,markers=None,markwhere=None,linestyle="solid",wait=0.5):
         #Gammla
-        plt.setp(plt.gca().lines, color="gray", linestyle="dotted", linewidth=1)
+        plt.setp(plt.gca().lines[2:], color="gray", linestyle="dotted", linewidth=1)
 
         #Nya
-        line = self.ax.plot(x, y, color="green",marker="x",markevery=[-1])
+        line = self.ax.plot(x, y, color=color ,marker=markers,markevery=markwhere,linestyle=linestyle)
 
         self.line.append(line)
         plt.draw()
-        plt.pause(1)
+        plt.pause(wait)
 
     def addScatter(self,x,y):
         line = self.ax.scatter(x, y)
@@ -41,24 +40,19 @@ class plot:
         plt.show(block=False)
         plt.pause(0.01)
 
-#a = plot(0,10,0,5)
-#a.addLine([0,1,2,3],[7,6,5,4])
-#a.addScatter([0,1,2,3],[1,3,1,3])
-#a.addLine([0,1,2,3],[1,3,1,3])
 
 
 
-def euler_forward_y(f,t_0,v_0,h,y_0):#Undersskattar värdet!
-    t_n = t_0
+def euler_forward_y(f,v_0,h):#Använd inte
+    t_n = 0
     v_n = v_0
     
-    integrate = y_0#in
+    integrate = 0#in
 
     #Plot stuff
-    t=[t_0]
     v=[v_0]
 
-    y=[y_0]#in
+    y=[0]#in
 
     i=0#in
     
@@ -66,26 +60,24 @@ def euler_forward_y(f,t_0,v_0,h,y_0):#Undersskattar värdet!
         v_n = v_n + h * f(v_n,t_n)
         t_n = t_n + h
         
-        t.append(t_n)
         v.append(v_n)
 
         integrate += h*(v[i+1]+v[i])/2#in
         i += 1#in
         y.append(integrate)#in
 
-    return t,v,y
+    return y
 
-def euler_forward_x(f,t_0,v_0,h,x_0,N):#Undersskattar värdet!
-    t_n = t_0
+def euler_forward_x(f,v_0,h,N):#Använd inte
+    t_n = 0
     v_n = v_0
     
-    integrate = 0
+    integrate = 0#in
     
     #Plot stuff
-    t=[0]
     v=[v_0]
     
-    x=[x_0]#in
+    x=[0]#in
 
     
     for i in range(N-1):
@@ -93,32 +85,49 @@ def euler_forward_x(f,t_0,v_0,h,x_0,N):#Undersskattar värdet!
         t_n = t_n + h
         
         #Plot stuff
-        t.append(t_n)
         v.append(v_n)
 
         
         integrate += h*(v[i+1]+v[i])/2#in
         x.append(integrate)#in
-    return t,v,x
+    return x
 
-b=float(input("Mål: "))
-k=float(input("k: "))
-m=float(input("m: "))
-a=plot(-1,b*1.5,-1,b)
-a.addScatter([b],[0])
-while True:
-    vx=float(input("x hastighet: "))
-    vy=float(input("y hastighet: "))
-    print()
+def euler_forward(vx,vy,k,m,h): #Inte för 2.3 men 2.5 funkar
+    y = euler_forward_y((lambda v,t:-k/m*v-9.82),vy,h) #-k/m*v-g
+    x = euler_forward_x((lambda v,t:-k/m*v),vx,h,len(y)) #-k/m*v-g
+    return x,y
+
+
+
+def gameInit(estimate): #estimate är uppskattningen med estimate(x speed when t=0,y speed when t=0,k,m,steglängd)
+    b=float(input("Mål: "))
+    k=float(input("k: "))
+    m=float(input("m: "))
+    hit=float(input("marginal: "))
+
     
-    t,v,y = euler_forward_y((lambda v,t:-k/m*v-9.82),0,vy,0.01,2) #-k/m*v-g
+    a=plot(-1,b*1.5,-1,b)
+    a.addScatter([0],[0])
+    a.addLine([-1,b*1.5],[0,0],"black")
+    a.addLine([b-hit,b+hit],[0,0],"black",2,[-1,0])
+
     
-    t,v,x = euler_forward_x((lambda v,t:-k/m*v),0,vx,0.01,0,len(y)) #-k/m*v-g
+    while True:
+        vx=float(input("x hastighet: "))
+        vy=float(input("y hastighet: "))
+        print()
+
+        #Sätt x,y uppskattningen här!
+        x,y=estimate(vx,vy,k,m,0.01)
     
-    a.addLine(x,y)
+        a.addLine(x,y,"green","x",[-1])
     
-    hit=2
-    if (b-hit<=x[-2]<=b+hit):
-        print("you win!")
-        break
+    
+        if (b-hit<=x[-1]<=b+hit):
+            print("you win!")
+            a.clear()
+            plt.close()
+            break
+
+gameInit(euler_forward)
     
